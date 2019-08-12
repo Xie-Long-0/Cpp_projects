902e671910864f285577de12ea5f744371ad9c03
XieLong <1293336945@qq.com>
2019-08-12 17:34:11 +0800

    done

--- 乘法表.c
+++ 乘法表.c
@@ -1,13 +1,11 @@
 /*      九九乘法表      */
 
 #include <stdio.h>
+
 int main()
 {
-	int a, b;
-	
-	for (a = 1; a <= 9; a++)
-	{
-		for (b = 1; b <= a; b++)
+	for (int a = 1; a <= 9; a++) {
+		for (int b = 1; b <= a; b++)
 			printf("%d×%d=%-2d ", b, a, a*b);
 		printf("\n");
 	}

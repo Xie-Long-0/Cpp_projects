3ecfe42aadbdfb5348c8e4d64be8713e8e7a14a4
XieLong <1293336945@qq.com>
2019-08-12 15:41:22 +0800

    done

--- 插入排序/main.c
+++ 插入排序/main.c
@@ -3,25 +3,34 @@
 
 #define LENGTH(a) (sizeof(a)/sizeof(a[0]))
 
+void print_array(int a[], const int n)
+{
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
 int main(void)
 {
     int s[] = {5, 8, 2, 1, 3};
-    int ns = LENGTH(s);
+    int ns  = LENGTH(s);
     int u[] = {10, 5, 6, 2, 3, 5, 2};
-    int nu = LENGTH(u);
+    int nu  = LENGTH(u);
+    int t[] = {5, 78, 100, 33, -6, 0, -20};
+    int nt  = LENGTH(t);
+    int w[] = {-4, -7, -8, 9, 6};
+    int nw  = LENGTH(w);
 
-    binary_insert_sort(s, ns, 0);
-    binary_insert_sort(u, nu, 1);
+    direct_insert_sort(s, ns, 0);
+    direct_insert_sort(u, nu, 1);
+    binary_insert_sort(t, nt, 0);
+    binary_insert_sort(w, nw, 1);
 
-    for (int i = 0; i < ns; ++i) {
-        printf("%d ", s[i]);
-    }
-    printf("\n");
-
-    for (int i = 0; i < nu; ++i) {
-        printf("%d ", u[i]);
-    }
-    printf("\n");
+    print_array(s, ns);
+    print_array(u, nu);
+    print_array(t, nt);
+    print_array(w, nw);
 
     return 0;
 }

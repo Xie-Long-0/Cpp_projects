4aca95abe4542ef71aa923a5c4bf5f34bd425a44
XieLong <1293336945@qq.com>
2021-03-19 17:51:35 +0800

    分解质因数.c 添加参数传入支持

--- 分解质因数.c
+++ 分解质因数.c
@@ -1,33 +1,47 @@
 #include <stdio.h>
-int main()
+#include <string.h>
+#include <stdlib.h>
+
+int main(int args, char *argv[])
 {
-	char ch[2048];
-	printf("请输入一个正整数：");
-	long long int i, n;
-	while ( !scanf("%lld", &n) || n<=0 )
-	{
-		gets(ch);
-		printf("输入错误！请输入一个正整数：");
-	}
-	if ( n==1 )
-	printf("1没有质因数。");
-	else
-	{
-		printf("%lld=", n);
-		for ( i=2; i<=n; i++ )
-		{
-			if ( n%i==0 )
-			{
-				if ( n==i )
-				printf("%lld", i);
-				else
-				{
-					printf("%lld*", i);
-					n/=i;
-					i=1;
-				}
-			}
-		}
-	}
-	return 0;
+    long long int i = 0, n = 0;
+    char ch[2048] = "\0";
+    if (args >= 2)
+    {
+        n = atoll(argv[1]); //将字符串转换为长整数
+    }
+    if (n <= 0)
+    {
+        printf("请输入一个正整数：");
+        while (!scanf("%lld", &n) || n <= 0)
+        {
+            gets(ch);   //用于清除输入缓冲
+            printf("输入错误！请输入一个正整数：");
+        }
+    }
+
+    if (n == 1)
+    {
+        printf("1没有质因数。\r\n");
+    }
+    else
+    {
+        printf("%lld = ", n);
+        for (i = 2; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                if (n == i)
+                    printf("%lld", i);
+                else
+                {
+                    printf("%lld*", i);
+                    n /= i;
+                    i = 1;
+                }
+            }
+        }
+        printf("\r\n");
+    }
+    return 0;
 }

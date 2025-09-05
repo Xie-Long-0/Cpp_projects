893c2b8cadb00fde6032ca172b9278903e604b80
XieLong <1293336945@qq.com>
2025-09-06 00:07:04 +0800

    优化Fibonacci代码

--- Fibonacci/fibo_itera.c
+++ Fibonacci/fibo_itera.c
@@ -1,16 +1,21 @@
+/**
+ * C17 版本的 Fibonacci 数列迭代实现
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-const int MAXSIZE = 50;
+const int MAXSIZE = 46;
 
 void fibonacci_iteration(int arr[], int len)
 {
-    int a = 0, b = 1, temp;
+    int a = 0, b = 1, temp = 0;
 
     if (arr == NULL || len <= 0)
         return;
-    else if (len > MAXSIZE)
-        len = MAXSIZE;
+    else if (len >= MAXSIZE)
+        len = MAXSIZE - 1;
 
     arr[0] = 1;
 
@@ -23,24 +28,36 @@ void fibonacci_iteration(int arr[], int len)
     }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    int n = 45; // 要计算的Fibonacci数列的项数
+    int n = MAXSIZE - 1; // 要计算的Fibonacci数列的项数
 
-    int *fi = (int *)malloc(sizeof(int) * MAXSIZE);
-    if (n > MAXSIZE)
+    if (argc > 1)
     {
-        n = MAXSIZE;
+        n = atoi(argv[1]);
     }
+    if (n >= MAXSIZE || n <= 0)
+    {
+        n = MAXSIZE - 1;
+    }
+
+    int fi[MAXSIZE];
+
+    clock_t time0 = clock();
 
     printf("Fibonacci 数列的前 %d 项为：\n", n);
     fibonacci_iteration(fi, n);
 
+    clock_t time1 = clock();
+
     for (int i = 0; i < n; i++)
     {
-        printf("%3d: %d\n", i + 1, fi[i]);
+        printf("%2d: %10d\n", i + 1, fi[i]);
     }
-	free(fi);
+
+    clock_t time2 = clock();
+
+    printf("Time used: %ld ms, %ld ms\n", time1 - time0, time2 - time1);
 
     return 0;
 }
--- Fibonacci/fibo_recur.c
+++ Fibonacci/fibo_recur.c
@@ -1,44 +1,63 @@
+/**
+ * C17 版本的 Fibonacci 数列递归实现
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-const int MAXSIZE = 45;
+const int MAXSIZE = 46;
 
 int fibonacci_recursion(int arr[], int n)
 {
-    if (arr == NULL || n == 0)
+    if (arr == NULL || n <= 0 || n >= MAXSIZE)
     {
         return 0;
     }
-    if (n == 1)
+
+    if (n <= 2)
     {
         arr[0] = 1;
-        return n;
+        arr[1] = 1;
+        return 1;
     }
     else
     {
-        arr[n - 1] = fibonacci_recursion(arr, n - 1) + fibonacci_recursion(arr, n - 2);
+        arr[n - 1] = fibonacci_recursion(arr, n - 1) + arr[n - 3];
         return arr[n - 1];
     }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    int n = 40; // 要计算的Fibonacci数列的项数
+    int n = MAXSIZE - 1; // 要计算的Fibonacci数列的项数
 
-    int *fi = (int *)malloc(sizeof(int) * MAXSIZE);
-    if (n > MAXSIZE)
+    if (argc > 1)
     {
-        n = MAXSIZE;
+        n = atoi(argv[1]);
     }
+    if (n >= MAXSIZE || n <= 0)
+    {
+        n = MAXSIZE - 1;
+    }
+
+    int fi[MAXSIZE];
+
+    clock_t time0 = clock();
 
     printf("Fibonacci 数列的前 %d 项为：\n", n);
     fibonacci_recursion(fi, n);
 
+    clock_t time1 = clock();
+
     for (int i = 0; i < n; i++)
     {
-        printf("%3d: %d\n", i + 1, fi[i]);
+        printf("%2d: %10d\n", i + 1, fi[i]);
     }
-    free(fi);
+
+    clock_t time2 = clock();
+
+    printf("Time used: %ld ms, %ld ms\n", time1 - time0, time2 - time1);
 
     return 0;
 }
